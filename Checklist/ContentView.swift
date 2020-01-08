41561803a0c95c064696b3e61e4011515984b875
sxc <qiuyequ@icloud.com>
2020-01-08 11:34:20 +0800

    add static content

--- Checklist/ContentView.swift
+++ Checklist/ContentView.swift
@@ -10,9 +10,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List {
+                Section(header: Text("Hight priority")) {
+                    Group {
+                        Text("Walk the dog")
+                        Text("Brush my teech")
+                        Text("Learn iOS development")
+                        Text("Walk the dog")
+                        Text("Brush my teech")
+                        Text("Learn iOS development")
+                        
+                    }
+                    
+                    
+                    Group {
+                        Text("Walk the dog")
+                        Text("Brush my teech")
+                        Text("Learn iOS development")
+                        Text("Learn iOS development")
+                        Text("Learn iOS development")
+                    }
+                    
+                    
+                }
+                Section(header: Text("Low priority")) {
+                    Text("Soccer practice")
+                    Text("Eat ice cream")
+                    Text("Eat ice cream")
+                    Text("Soccer practice")
+                    Text("Eat ice cream")
+                    Text("Eat ice cream")
+                    Text("Soccer practice")
+                    Text("Eat ice cream")
+                    Text("Eat ice cream")
+                    Text("Eat ice cream")
+                }
+                
+            }
+        .listStyle(GroupedListStyle())
+        .navigationBarTitle("Checklist")
+        }
     }
+    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
